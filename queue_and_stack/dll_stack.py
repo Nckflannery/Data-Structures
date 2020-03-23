1fb2a063add9f3c0fa19dab8a7447443accf2b43
Nckflannery <Nckflannery@users.noreply.github.com>
2020-03-23 15:01:54 -0700

    Backwards!

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,7 +6,7 @@ class Stack:
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        self.storage.add_to_tail(value)
+        self.storage.add_to_head(value)
         self.size += 1
 
     def pop(self):
@@ -14,7 +14,7 @@ class Stack:
             return None
         else:
             self.size -= 1
-            return self.storage.remove_from_tail()
+            return self.storage.remove_from_head()
 
     def len(self):
         return self.size
